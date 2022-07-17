f1bbca13f101265b2cf7c8597f978e1b3cbde8fa
Daniil Goncharov <brovar002@gmail.com>
2022-07-17 16:55:03 +0300

    feat: add first and second tasks

---
+++ DesktopTask/src/main/java/by/goncharov/desktoptask/Controller.java
@@ -0,0 +1,106 @@
+package by.goncharov.desktoptask;
+
+import by.goncharov.desktoptask.entities.Counter;
+import by.goncharov.desktoptask.entities.ListUsr;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+    private ResourceBundle resourceBundle;
+    private ListUsr list = new ListUsr();
+    @FXML
+    private TextField listElement;
+    @FXML
+    private Label addText;
+    @FXML
+    private Label listAll;
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Label countText;
+    @FXML
+    private Label classCount;
+    @FXML
+    private Label counter;
+    @FXML
+    private Label sizeText;
+    @FXML
+    private Label sizeLabel;
+    @FXML
+    private Label listUsrLabel;
+    @FXML
+    private Button selectButton;
+    @FXML
+    private Button counterButton;
+    @FXML
+    private Button listusrButton;
+    @FXML
+    private Menu menu;
+
+    @FXML
+    private MenuItem englishItem;
+
+    @FXML
+    private MenuItem russianItem;
+
+
+    @FXML
+    void initialize() {
+        loadLang("en");
+    }
+    @FXML
+    private void setRussianLang() {
+        loadLang("ru");
+    }
+
+    @FXML
+    private void setEnglishLang() {
+        loadLang("en");
+    }
+
+    @FXML
+    private void welcome() {
+        welcomeText.setText(resourceBundle.getString("welcome.message"));
+    }
+    @FXML
+    private void counter() {
+        new Counter();
+    classCount.setText(String.valueOf(Counter.getClassCount()));
+    }
+    @FXML
+    private void addList() {
+       list.add(listElement.getText());
+        sizeLabel.setText(String.valueOf(list.getSize()));
+        listAll.setText(list.show());
+    }
+
+    private void loadLang(String lang) {
+        Locale locale = new Locale(lang);
+        resourceBundle = ResourceBundle.getBundle("messages", locale);
+        welcomeText.setText("");
+        initializeElements();
+    }
+
+    private void initializeElements() {
+        selectButton.setText(resourceBundle.getString("select"));
+        menu.setText(resourceBundle.getString("language.change"));
+        englishItem.setText(resourceBundle.getString("lang.eng"));
+        russianItem.setText(resourceBundle.getString("lang.ru"));
+        counterButton.setText(resourceBundle.getString("back"));
+        listusrButton.setText(resourceBundle.getString("back"));
+        counter.setText(resourceBundle.getString("counter"));
+        addText.setText(resourceBundle.getString("count.add"));
+        countText.setText(resourceBundle.getString("count"));
+        sizeText.setText(resourceBundle.getString("list.size"));
+        listUsrLabel.setText(resourceBundle.getString("listusr"));
+    }
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
